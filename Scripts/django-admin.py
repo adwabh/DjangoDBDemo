fdc40689b3f47d659cd8968fabc0eed37b74939b
Adwait Abhyankar <avabhyankar22@gmail.com>
2018-10-30 16:02:20 +0530

    lecture 18 complete;code for section 4 complete

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\nex7ngg\documents\python\pythonrest\django04\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
